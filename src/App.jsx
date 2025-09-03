1c6d6e6f26cb35f19e1cfbbe87a82ad8b5e8ec9f
Shivam-Vermacs <verma.shivam.cs@gmail.com>
2025-09-04 05:08:44 +0530

    Created a Timer Component and learnt about Cleanup Function and useEffect and Mounting and unmounting and also how to make the data of the timer persist even after re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TodoList } from "./Components/TodoList";
 import { NavbarMiniDemo } from "./Components/NavBarmini-demo";
 
 function App() {
-  return (
-    <div>
-      <NavbarMiniDemo />
-    </div>
-  );
+  const [showtimer, setShowTimer] = useState(true);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setShowTimer((value) => !value), 500);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setCounter((count) => count + 1),
+      1000
+    );
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return <div>{showtimer && <Timer counter={counter} />}</div>;
+}
+
+function Timer({ counter }) {
+  return <div>{counter} Seconds</div>;
 }
 
 export default App;
