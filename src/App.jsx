ece447e31f0f70b5f69035d5cb204d7edddf540a
Shivam-Vermacs <verma.shivam.cs@gmail.com>
2025-09-04 07:41:27 +0530

    Learned About Children Props in React and Practiced Passing Children Props to a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import React, { useEffect, useState } from "react";
 import { TodoList } from "./Components/TodoList";
 import { NavbarMiniDemo } from "./Components/NavBarmini-demo";
 
 function App() {
-  const [showtimer, setShowTimer] = useState(true);
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => setShowTimer((value) => !value), 500);
-    return () => clearInterval(intervalId);
-  }, []);
-
-  useEffect(() => {
-    const intervalId = setInterval(
-      () => setCounter((count) => count + 1),
-      1000
-    );
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return <div>{showtimer && <Timer counter={counter} />}</div>;
+  return (
+    <div style={{ backgroundColor: "#dadbdbff", minHeight: "100vh" }}>
+      <Card>
+        <div>
+          <h1>Card</h1>
+          <textarea style={{ width: "400px" }} type="text" />
+        </div>
+      </Card>{" "}
+      {/*passing the inside content as children*/}
+    </div>
+  );
 }
 
-function Timer({ counter }) {
-  return <div>{counter} Seconds</div>;
+function Card({ children }) {
+  return (
+    <div
+      style={{
+        fontFamily: "sans-serif",
+        margin: "20px",
+        border: "1.5px solid #e6e6e7ff",
+        color: "black",
+        backgroundColor: "#ffffffff",
+        borderRadius: "5px",
+        padding: "20px",
+        width: "50%",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default App;
