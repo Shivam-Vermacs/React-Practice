e376c08de1ff5013329f4acd9c060b208fddb2f4
Shivam-Vermacs <verma.shivam.cs@gmail.com>
2025-09-13 18:16:21 +0530

    Rolling up the state Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,40 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-  Outlet,
-} from "react-router-dom";
+import { useState } from "react";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<LandingHome />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <div>
+        <LightBulb />
+      </div>
     </>
   );
 }
 
-const ErrorPage = () => {
+function LightBulb() {
+  const [BulbOn, setBulbOn] = useState(false);
   return (
     <>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <BulbState BulbOn={BulbOn} />
+      <Switch setBulbOn={setBulbOn} />
     </>
-  );
-};
-const Layout = () => {
-  return (
-    <>
-      <Link to="/">Home</Link>|<Link to="/contact">Contact</Link>|
-      <Link to="/about">AboutUs</Link>
-      <Outlet />
-      <br />
-      <strong>Footer</strong>
-    </>
-  );
-};
-
-const LandingHome = () => {
-  return (
-    <>
-      <h1>Why can't orphans play baseball? They never find ‘HOME'</h1>
-    </>
-  );
-};
-const Contact = () => {
-  const navigate = useNavigate();
-
-  function redirect() {
-    navigate("/");
-  }
-  return (
-    <>
-      <h1>CONTACT PAGE</h1>
-      <button onClick={redirect}>Back To Home</button>
-    </>
-  );
-};
-const AboutUs = () => {
-  return (
-    <>
-      <h1>About us: sleep is good, but eternal rest? Even better</h1>
-    </>
-  );
-};
-
-function Card({ children }) {
-  return (
-    <div
-      style={{
-        fontFamily: "sans-serif",
-        margin: "20px",
-        border: "1.5px solid #e6e6e7ff",
-        color: "black",
-        backgroundColor: "#ffffffff",
-        borderRadius: "5px",
-        padding: "20px",
-        width: "50%",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      {children}
-    </div>
   );
 }
 
+function BulbState({ BulbOn }) {
+  return (
+    <>
+      <div>{BulbOn ? "💡" : " 💤"}</div>
+    </>
+  );
+}
+
+function Switch({ setBulbOn }) {
+  return (
+    <>
+      <div>
+        <button onClick={() => setBulbOn((current) => !current)}>Toggle</button>
+      </div>
+    </>
+  );
+}
 export default App;
