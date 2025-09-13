dfbf084ce684f1836e4ce80926777d0f14479d42
Shivam-Vermacs <verma.shivam.cs@gmail.com>
2025-09-13 19:24:22 +0530

    Learned About Prop drilling and Context Api and how context api handles and fixes the issues prop drilling introduces

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from "react";
+import { createContext, useState, useContext } from "react";
 
+// Create Context for Bulb state
+const BulbOnContext = createContext();
 function App() {
   const [BulbOn, setBulbOn] = useState(false);
   return (
     <>
       <div>
-        <LightBulb BulbOn={BulbOn} setBulbOn={setBulbOn} />
+        {/* Provide the Bulb state and setter to the context */}
+        <BulbOnContext.Provider value={{ BulbOn, setBulbOn }}>
+          <LightBulb />
+        </BulbOnContext.Provider>
       </div>
     </>
   );
 }
 
-function LightBulb({ BulbOn, setBulbOn }) {
+function LightBulb() {
   return (
     <>
-      <BulbState BulbOn={BulbOn} />
-      <Switch setBulbOn={setBulbOn} />
+      <BulbState />
+      <Switch />
     </>
   );
 }
 
-function BulbState({ BulbOn }) {
+function BulbState() {
+  // Consume the Bulb state from context
+  const { BulbOn } = useContext(BulbOnContext);
   return (
     <>
-      <div>{BulbOn ? "💡" : " 💤"}</div>
+      <div>{BulbOn ? "Bulb on" : " Bulb off"}</div>
     </>
   );
 }
-
-function Switch({ setBulbOn }) {
+// Consume the setter function from context to toggle the Bulb state
+function Switch() {
+  const { setBulbOn } = useContext(BulbOnContext);
   return (
     <>
       <div>
